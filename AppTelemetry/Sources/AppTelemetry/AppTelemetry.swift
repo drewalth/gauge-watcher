c84d2919b2b1c5282789861a8a0e1cc83e95a44e
drewalth <16584198+drewalth@users.noreply.github.com>
2025-11-29 15:54:43 -0700

    fix: apptel init

--- AppTelemetry/Sources/AppTelemetry/AppTelemetry.swift
+++ AppTelemetry/Sources/AppTelemetry/AppTelemetry.swift
@@ -1,19 +1,50 @@
+import Foundation
 import os
 import PostHog
 
 public enum AppTelemetry {
-    private static let logger = Logger(subsystem: "com.drewalth.GaugeWatcher", category: "AppTelemetry")
+
+    // MARK: Public
+
     public static func initialize() {
-        let posthogAPIKey = ""
-        // usually 'https://us.i.posthog.com' or 'https://eu.i.posthog.com'
-        let posthogHost = ""
+        let config = loadConfig()
+        let posthogAPIKey = config["POSTHOG_API_KEY"] ?? ""
+        let posthogHost = config["POSTHOG_HOST"] ?? ""
+
+        logger.info("PostHog API Key: \(posthogAPIKey)")
+        logger.info("PostHog Host: \(posthogHost)")
 
         if posthogHost.isEmpty || posthogAPIKey.isEmpty {
             logger.warning("Missing PostHog credentials")
             return
         }
 
-        let config = PostHogConfig(apiKey: posthogAPIKey, host: posthogHost)
-        PostHogSDK.shared.setup(config)
+        let posthogConfig = PostHogConfig(apiKey: posthogAPIKey, host: posthogHost)
+        PostHogSDK.shared.setup(posthogConfig)
     }
+
+    public static func captureEvent(_ name: String) {
+        PostHogSDK.shared.capture(name)
+    }
+
+    // MARK: Private
+
+    private static let logger = Logger(subsystem: "com.drewalth.GaugeWatcher", category: "AppTelemetry")
+
+    private static func loadConfig() -> [String: String] {
+        guard
+            let configURL = Bundle.module.url(forResource: "config", withExtension: "plist"),
+            let data = try? Data(contentsOf: configURL),
+            let plist = try? PropertyListSerialization.propertyList(from: data, format: nil) as? [String: Any]
+        else {
+            logger.error("Failed to load config.plist")
+            return [:]
+        }
+
+        return [
+            "POSTHOG_API_KEY": plist["POSTHOG_API_KEY"] as? String ?? "",
+            "POSTHOG_HOST": plist["POSTHOG_HOST"] as? String ?? ""
+        ]
+    }
+
 }
